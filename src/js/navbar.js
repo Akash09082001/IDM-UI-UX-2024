1983235bf1ac4b2d2f3b384cea6d754b2491a58d
Akash09082001 <aakashl@letsupgrade.in>
2024-03-23 19:00:47 +0530

    added new cahnge

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -74,3 +74,22 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+
+// menu open and close
+
+const mobileMenu = document.getElementById("mobile-nav");
+const openButton = document.getElementById("open");
+const closeButton = document.getElementById("close");
+
+// open menu
+function openMenu() {
+    mobileMenu.style.display = "flex";
+    openButton.style.display = "none";
+    closeButton.style.display = "block";
+}
+function closeMenu() {
+    mobileMenu.style.display = "none";
+    openButton.style.display = "block";
+    closeButton.style.display = "none";
+}
